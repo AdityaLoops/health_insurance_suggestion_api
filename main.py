f6f272b2aae4d00f08731dd660841ba029a32a08
Aditya Raj Verma <verma1980aditya@gmail.com>
2026-08-15 02:15:11 +0530

    Built ML Pipeline and prediction API

--- main.py
+++ main.py
@@ -1,3 +1,44 @@
 import joblib
 from fastapi import FastAPI, HTTPException
 import pandas as pd
+from pydantic import BaseModel, Field
+from typing import Literal
+
+app = FastAPI(
+    title = "Health Insurance Suggestion API",
+    description = "API for prediction health insurance charges using a trained Random Forest Regressor model",
+    version ="1.0.0"
+)
+
+model = joblib.load("insurance_model.joblib")
+
+class InsuranceFeatures(BaseModel):
+    age: int = Field(gt=0, le=110, description="Age of the individual")
+    sex: Literal["male", "female"] = Field(description ="Sex of the individual")
+    bmi: float = Field(gt=15, le=54, description="Body Mass Index of the individual")
+    children: int = Field(ge=0, le=5, description="Number of children of the individual")
+    smoker: Literal["yes", "no"] = Field(description="Is the individual a smoker?")
+    region: Literal["southwest", "southeast", "northwest", "northeast"] = Field(description="Region of the individual")
+
+
+@app.get("/")
+def home():
+    return {"message": "Health Insurance Suggestion API",
+            "status": "running",
+            "docs" : "/docs"}
+
+@app.get("/health")
+def health():
+    return {
+        "status": "healthy",
+        "model": "Random forest Regressor",
+        "model loaded": True
+    }
+
+@app.post("/predict")
+def predict(features: InsuranceFeatures):
+    input_data = pd.DataFrame([features.model_dump()])
+    prediction = model.predict(input_data)[0]
+    return {
+        "predicted charge": round(float(prediction),2)
+    }
--- train_model.py
+++ train_model.py
@@ -1,11 +1,11 @@
 import joblib
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.metrics import mean_absolute_error, r2_score
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.pipeline import Pipeline 
 # this transformer will help us to apply one hot encoding to categorical columns and leave the numerical
 # columns as it is. We will use this transformer in our pipeline
 
@@ -19,48 +19,18 @@ x_train, x_test, y_train, y_test = train_test_split(x,y, test_size=0.2, random_s
 
 encoder = OneHotEncoder(handle_unknown="ignore")
 
-# x_train_encoded = encoder.fit_transform(x_train).ColumnTransformer("sex", "smoker", "region")     WRONG
-
 preprocessor = ColumnTransformer( [(" categorical" ,encoder, ["sex", "smoker", "region"])], remainder="passthrough")
 
-x_train_encoded = preprocessor.fit_transform(x_train)
-x_test_encoded = preprocessor.transform(x_test)
-# we used only TRANSFORM on x_test bc the encoder has learned the encoding from the training data .
-# we don't want to fit the encoder again on x_test.
-
-model = LinearRegression()
-model.fit(x_train_encoded, y_train)
-
-y_pred = model.predict(x_test_encoded)
-
-mae = mean_absolute_error(y_test, y_pred)
-r2= r2_score(y_test, y_pred)
-
-print(f"MAE: {mae}")
-print(f"R2: {r2}")
-
-# CHECK FOR OVERFITTING 
-train_pred = model.predict(x_train_encoded)
-train_r2 = r2_score(y_train, train_pred)
-test_r2 = r2_score(y_test, y_pred)
-
-print(f"Train R2: {train_r2:.4f}")
-print(f"Test R2: {test_r2:.4f}")
-# The results show us that the model is not overfitting 
-
-
 model = RandomForestRegressor(n_estimators=200, random_state=42)
-model.fit(x_train_encoded, y_train)
-y_pred=model.predict(x_test_encoded)
+pipeline = Pipeline([("preprocessor", preprocessor), ("model", model)])
+pipeline.fit(x_train, y_train)
+y_pred = pipeline.predict(x_test)
 mae = mean_absolute_error(y_test, y_pred)
 r2 = r2_score(y_test, y_pred)
 
 print(f"MAE: {mae}")
 print(f"R2: {r2}")
 
-train_pred = model.predict(x_train_encoded)
 
-train_r2 = r2_score(y_train, train_pred)
-
-print(f"Train R2: {train_r2:.4f}")
-print(f"Test R2: {r2:.4f}")
+joblib.dump(pipeline, "insurance_model.joblib")
+print("Model saved as insurance_model.joblib")
